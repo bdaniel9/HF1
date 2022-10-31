c09f014e7a0481c978f2626fc2a75850ad3eacc5
Daniel Bochis <bochisdaniel3@gmail.com>
2022-10-31 10:37:58 +0100

    Páros számok ki íratása

--- src/main/java/homework1.java
+++ src/main/java/homework1.java
@@ -5,5 +5,11 @@ public class homework1 {
             System.out.println("Hello Debrecen!");
         }
         System.out.println("Ezt a szöveget a második branch-hez adtam hozzá.");
+        System.out.println("Páros számok: ");
+        for (int x=1;x<11;x++){
+            if (x%2==0){
+                System.out.println(x);
+            }
+        }
     }
 }
