43afee6a2ef89bfbbcb347b1ec3817eed4670eb1
Daniel Bochis <bochisdaniel3@gmail.com>
2022-10-31 10:33:27 +0100

    Added for loop + Hello Debrecen

--- src/main/java/homework1.java
+++ src/main/java/homework1.java
@@ -1,5 +1,8 @@
 public class homework1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for (int i=0;i<2;i++){
+            System.out.println("Hello Debrecen!");
+        }
     }
 }
